3f68bb473ff443c34618e4fdb51e594e76c418f9
krishna bhardwaj <krishnabhardwaj5427912@gmail.com>
2025-10-04 00:33:35 +0530

    Combination sum

--- Assignment6Oct3/Combination_Sum_II.java
+++ Assignment6Oct3/Combination_Sum_II.java
@@ -1,7 +1,35 @@
 package Assignment6Oct3;
 
+import java.util.*;
+
 public class Combination_Sum_II {
     public static void main(String[] args) {
-        
+        int[] candidate={10,1,2,7,6,1,5};
+        int target=8;
+        List<List<Integer>> ans=combinationSum2(candidate,target);
+        for (List<Integer> ll:ans){
+            System.out.println(ll);
+        }
+    }
+    static List<List<Integer>> ans;
+    public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        ans=new ArrayList<>();
+        List<Integer> ll=new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(ll,candidates,target,0);
+        return ans;
+    }
+    public static void helper(List<Integer> ll,int[] candi,int target,int idx){
+        if(target==0){
+            ans.add(new ArrayList<>(ll));
+            return;
+        }
+        for(int i=idx;i<candi.length;i++){
+            if(i>idx && candi[i]==candi[i-1]) continue;
+            if(candi[idx]>target) break;
+            ll.add(candi[i]);
+            helper(ll,candi,target-candi[i],i+1);
+            ll.remove(ll.size()-1);
+        }
     }
 }
